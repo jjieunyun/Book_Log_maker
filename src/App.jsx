f8a61771716501dc42340c78e8f3b606c688d259
jieun yun <lesson0186@gmail.com>
2022-05-31 17:25:14 +0900

    재배포

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import styles from './App.module.css';
 import Login from './components/login/login';
 import Maker from './components/maker/maker';
@@ -7,13 +7,13 @@ import Maker from './components/maker/maker';
 function App({FileInput ,authService, cardRepository}) {
   return (
     <div className={styles.app}>
-      <BrowserRouter basename={'https://jjieunyun.github.io/Book_Log_maker/'}>
+      <HashRouter>
         <Routes>
           <Route exact path='/' element={<Login authService={authService}/>} />
           <Route path='/maker' element={<Maker FileInput={FileInput} authService={authService} 
                 cardRepository={cardRepository} />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
